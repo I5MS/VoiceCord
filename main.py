d7191e93520c0879ecbc9b7a32254e98750f87ce
Dots <real5ms.og@gmail.com>
2024-08-13 06:07:14 +0400

    Removed non-functional print statement related to voice channel connection.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ async def on_ready():
     print('Logged in as {0.user}'.format(client))
     voice_channel = client.get_channel(1111111111111111111)
     await voice_channel.connect()
-    print('Connected to voice channel: {}'.format(voice_channel))
 
 
 client.run("Your_Token_Here", bot=False)
